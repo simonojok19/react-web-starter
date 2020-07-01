493b0a499971754baabbacfc78823c6c15332742
simonojok19 <simonojok19@gmail.com>
2020-07-01 22:11:33 +0300

    done

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: "",
+    };
+  }
+  handleClick(e) {
+    const { value } = e.target;
+    this.setState({ newTodo: value });
+  }
+
+  handleC(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({ todos, newTodo: "" });
+  }
+
+  removeTodo(i) {
+    const todos = [
+      ...this.state.todos.slice(0, i),
+      ...this.state.todos.slice(i + 1),
+    ];
+    this.setState({ todos });
+  }
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="New Todo"
+            value={this.state.newTodo}
+            onChange={this.handleClick.bind(this)}
+          />
+          <button onClick={this.handleC.bind(this)}>Create</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo, i) => (
+            <li key={i} onClick={() => this.removeTodo.call(this, i)}>
+              {todo}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
